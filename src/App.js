67b2e6200a21f6d2f4ebcd0bab2ba31760c95dc7
github yami654 <github yami654@naver.com>
2021-07-15 21:36:03 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 import MainPageComponent from "./main";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />;
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,24 @@
+import React from "react";
 import "./index.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://946a6356-8876-4b70-b153-220276331076.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러발생 : ", error);
+      });
+  }, []);
+
   return (
     <div>
       <div id="header">
@@ -14,22 +32,28 @@ function MainPage() {
         </div>
         <h1>판매되는 상품들</h1>
         <div id="product-list">
-          <div className="product-card">
-            <div>
-              <img
-                className="product-img"
-                src="images/products/keyboard1.jpg"
-              />
-            </div>
-            <div className="product-contents">
-              <span calssName="product-name">키보드</span>
-              <span calssName="product-price">50000원</span>
-              <div calssName="product-seller">
-                <img className="product-avatar" src="images/icons/avatar.png" />
-                <span>그랩</span>
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <Link className="product-link" to={`/products/${index}`}>
+                  <div>
+                    <img className="product-img" src={product.imageUrl} />
+                  </div>
+                  <div className="product-contents">
+                    <span calssName="product-name">{product.name}</span>
+                    <span calssName="product-price">{product.price}원</span>
+                    <div calssName="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
       <div id="footer"></div>
